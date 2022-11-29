2eeb934aa64dc3dd86369060ec9412f6bc1c175d
wangjian <wangjian_1998@outlook.com>
2022-11-29 16:45:11 +0800

    更多歌曲页面

---
+++ components/song-list/index.js
@@ -0,0 +1,30 @@
+// components/song-list/index.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    index: {
+      type: Number,
+      value: 1
+    },
+    itemData: {
+      type: Object,
+      value: {}
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
--- pages/detail-songs/index.js
+++ pages/detail-songs/index.js
@@ -1,25 +1,35 @@
 // pages/detail-songs/index.js
+import {hyEventStore} from '../../store/index'
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    ranking: '',
+    rankingInfo: {}
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
-    console.log(options.ranking)
+    const ranking = options.ranking
+    this.setData({ranking})
+
+    hyEventStore.onState(this.data.ranking, this.getData)
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload() {
-
+    hyEventStore.offState(this.data.ranking, this.getData)
   },
+
+  getData: function(res) {
+    this.setData({rankingInfo: res})
+    console.log(this.data.rankingInfo)
+  }
 
 })
